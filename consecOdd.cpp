2ab9f193e7416ab897efd043673eec3b373a6dd9
Abhishek Pal <palabhishek085@gmail.com>
2026-07-17 22:05:23 +0530

    Sum of consecutive Odd

---
+++ consecOdd.cpp
@@ -0,0 +1,18 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int sum=0;
+        for(int i = min(a,b)+1 ; i < max(a,b) ; i++){
+            if(i%2!=0){
+                sum+=i;
+            }
+        }
+        cout<<sum<<" "<<endl;
+    }   
+}
