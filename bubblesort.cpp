b13931a156210e219b798184ac5fd9c89ed1f7f4
Abhishek Pal <palabhishek085@gmail.com>
2026-08-26 22:30:46 +0530

    Implementing the bubble sort algorithm

---
+++ bubblesort.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  int arr[5]={64,25,12,22,11};
+
+  for(int i=0;i<5;i++){
+    for(int j=i+1;j<5;j++){
+      if(arr[i]>arr[j]){
+        swap(arr[i],arr[j]);
+      }
+    }
+  }
+
+  for(int i=0;i<5;i++){
+    cout<<arr[i]<<" ";
+  }
+}
