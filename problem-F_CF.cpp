5c6e756f38be1ddf216767d1e98ee959c13b9271
Abhishek Pal <palabhishek085@gmail.com>
2026-09-14 19:19:25 +0530

    CF contest problem-F

---
+++ problem-F_CF.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int left=0,right=n-1;
+
+
+    while(left<=right ){
+        if(left==right){
+            cout<<arr[left]<<" ";
+        }
+        else{
+            cout<<arr[left]<<" "<<arr[right]<<" ";
+        }
+        left++;
+        right--;
+    }
+}
